171f984268655e75c655d57647cfbfec6c5b8a95
Neha Choudhari <npc@andrew.cmu.edu>
2022-11-01 19:08:18 -0400

    Completed in class exercise

--- Repos_Async_Class_Starter.playground/Contents.swift
+++ Repos_Async_Class_Starter.playground/Contents.swift
@@ -21,9 +21,9 @@ struct Repository: Decodable {
 }
 
 // Our initial function (needs fixing...)
-func fetchRepositories() throws -> [Repository] {
+func fetchRepositories() async throws -> [Repository] {
   let url = URL(string: "https://api.github.com/search/repositories?q=language:swift&sort=stars&order=desc")!
-  let (data, _) = try URLSession.shared.data(from: url)
+  let (data, _) = try await URLSession.shared.data(from: url)
   return try JSONDecoder().decode(Repositories.self, from: data).repos
 }
 
@@ -31,11 +31,23 @@ func fetchRepositories() throws -> [Repository] {
 // A task to utilize this function
 // - call function then loop over repos array to print out the name & url for each
 // - interject lots of print statements with 'Step X' to see how things are progressing
+
 Task {
   print("Step 1")
+  
+  do {
+    let repos  = try await fetchRepositories()
+    for repo in repos
+    {
+      print("Step 2")
+      print(repo.name)
+      print(repo.htmlURL)
+    }
+  }
+  catch {
+    print(error)
+  }
 
-  // ...
-
-  print("Step 5")
+  print("Step 3")
 }
-print("Step 6")
+print("Step 4")
